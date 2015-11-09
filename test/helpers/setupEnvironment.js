e8bfe1c461bb313c3f7a585c30f5607e231c4157
troy betz <me@troybetz.com>
2015-11-09 13:46:51 -0600

    Rewrite tests
    Use Mocha & JSDom instead of jest, also added some test helpers

---
+++ test/helpers/setupEnvironment.js
@@ -0,0 +1,9 @@
+/**
+ * Module dependencies
+ */
+
+import { jsdom } from 'jsdom';
+
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
---
+++ test/helpers/shallowRender.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies
+ */
+
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import setupYouTube from './setupYouTube';
+
+/**
+ * Helper for testing the output of a component's `render` method
+ *
+ * @param {Object} [props] - component instance props
+ * @returns {Object}
+ */
+
+const shallowRender = (props) => {
+  const { YouTube } = setupYouTube();
+
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<YouTube { ...props }/>);
+
+  const output = renderer.getRenderOutput();
+
+  return {
+    props,
+    output,
+  };
+};
+
+export default shallowRender;
