d81a07ad86d84961661f719a3e13d8c27f68586b
tjallingt <tjallingt@gmail.com>
2020-04-19 16:49:55 +0200

    chore: make example work with codesandbox ci

---
+++ example/src/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import YouTube from 'react-youtube';
+
+import './styles.css';
+
+const VIDEOS = ['XxVg_s8xAms', '-DX3vJiqxm4'];
+
+function YouTubeComponentExample() {
+  const [player, setPlayer] = useState(0);
+  const [videoIndex, setVideoIndex] = useState(0);
+  const [width, setWidth] = useState(600);
+  const [hidden, setHidden] = useState(false);
+  const [autoplay, setAutoplay] = useState(false);
+
+  return (
+    <div className="App">
+      <div style={{ display: 'flex', marginBottom: '1em' }}>
+        <button type="button" onClick={() => player.seekTo(120)}>
+          Seek to 2 minutes
+        </button>
+        <button type="button" onClick={() => setVideoIndex((videoIndex + 1) % VIDEOS.length)}>
+          Change video
+        </button>
+        <label>
+          <input
+            type="range"
+            min="300"
+            max="1080"
+            value={width}
+            onChange={(event) => setWidth(event.currentTarget.value)}
+          />
+          Width ({width}px)
+        </label>
+        <button type="button" onClick={() => setHidden(!hidden)}>
+          {hidden ? 'Show' : 'Hide'}
+        </button>
+        <label>
+          <input type="checkbox" value={autoplay} onChange={(event) => setAutoplay(event.currentTarget.checked)} />
+          Autoplaying
+        </label>
+      </div>
+
+      {hidden ? (
+        'mysterious'
+      ) : (
+        <YouTube
+          videoId={VIDEOS[videoIndex]}
+          autoplay={autoplay}
+          width={width}
+          height={width * (9 / 16)}
+          className="container"
+          onReady={(event) => setPlayer(event.target)}
+        />
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<YouTubeComponentExample />, document.getElementById('app'));
